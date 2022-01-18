9a2d7ea3bf0303d4680c6479f670e05c00d4b8f9
lstubdal <lotte.stubdal@gmail.com>
2022-01-18 10:58:55 +0100

    Importere database.js til Articles

--- src/components/ArticlePreview.vue
+++ src/components/ArticlePreview.vue
@@ -1,8 +1,21 @@
 <template>
-   article previews
+   <div class="article-preview">preview
+      <div class="article-preview__title">preview title</div>
+      <div class="article-preview__lead">preview lead</div>
+      <div class="article-preview__image">preview image</div>
+   </div>
 </template>
 
 <script>
+   import Database from '../database.js';
+
+   export default {
+      data() {
+         return {
+            Database
+         }
+      }
+   }
 
 </script>
 
