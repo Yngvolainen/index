6f47e2366d58477c53320f3fca825e46ad0856b8
judith lourde <judith.lourde@gmail.com>
2022-01-19 00:08:23 +0100

    Added database to app.js

--- src/app/app.js
+++ src/app/app.js
@@ -4,10 +4,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 
-import store from '../store.js'
+// import store from '../store.js'
 import routes from '../routes.js';
+import database from '../database.js';
 
-const app_store = createStore(store);
+const app_store = createStore(database);
 const app_router = createRouter({
 	routes: routes,
 	history: createWebHistory()
