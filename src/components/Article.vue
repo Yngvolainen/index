4f71f6de6526fe92d6818954f47cb9cf7430eec5
lstubdal <lotte.stubdal@gmail.com>
2022-01-18 10:58:55 +0100

    Importere database.js til Articles

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,9 +1,23 @@
 <template>
-   article component
+   <div class="article">
+      article component
+      <div class="article__title">title</div>
+      <div class="article__img">img</div>
+      <div class="article__text">text</div>
+   </div>
+
 </template>
 
 <script>
+   import Database from '../database.js';
 
+   export default {
+      data() {
+         return {
+            Database
+         }
+      }
+   }
 </script>
 
 <style>
--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,9 +1,20 @@
 <template>
    articleS
+    <!-- loop med previews som igjen inneholder articler
+                     - loop med articlene  -->
+   <div> test henting av tittel: {{ Database.articles[0].title }}</div>
 </template>
 
 <script>
+   import Database from '../database.js';
 
+   export default {
+      data() {
+         return {
+            Database
+         }
+      }
+   }
 </script>
 
 <style>
