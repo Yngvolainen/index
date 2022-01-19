1d12bc163404dc34e6e8bc4c02e06f855be52c8a
lstubdal <lotte.stubdal@gmail.com>
2022-01-19 12:34:47 +0100

    Create database as store

--- src/app/app.js
+++ src/app/app.js
@@ -4,10 +4,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 
-import store from '../store.js'
+import database from '../database.js'
 import routes from '../routes.js';
 
-const app_store = createStore(store);
+const app_store = createStore(database);
 const app_router = createRouter({
 	routes: routes,
 	history: createWebHistory()
