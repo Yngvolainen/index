38e57f7221798944615113a92f2eafcf64f64940
Yngvolainen <yngve.josevold@gmail.com>
2022-01-19 15:32:06 +0100

    add basic structure

--- src/components/Journal.vue
+++ src/components/Journal.vue
@@ -1,5 +1,13 @@
 <template>
-  
+    <div class="journal">
+        <div class="journal__header">
+            <h2>Journal</h2>
+        </div>
+
+        <div>
+            
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +17,7 @@ export default {
 </script>
 
 <style>
-
+    .journal__header {
+        text-align: center;
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,12 @@
 <template>
-	<div>:~)</div>
+	<Journal />
 </template>
+
+<script>
+import Journal from '../components/Journal.vue'
+export default {
+	components: {
+		Journal
+	},
+}
+</script>
