baa86ad003b5fb769e002fbbf29f94ed6f255644
mimmimiro <amini.maryam@gmail.com>
2022-01-19 09:49:25 +0100

    commit

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,7 +1,15 @@
 <template>
 	<RouterView />
+	<Footer />
 </template>
-
+<script>
+import Footer from '../components/Footer.vue';
+export default {
+	components: {
+		Footer, 
+	}
+}
+</script>
 <style>
 	@import '../style/reset.css';
 	@import '../style/variables.css';
---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+<section class="footer">
+
+<h3 class="footer__title">in(dex)</h3>
+
+<nav class="footer__navigation">
+	<p class="footer__navigation-link">About us</p> 
+	<p class="footer__navigation-social">Instagram</p> 
+	<p class="footer__navigation-social">Twitter</p> 
+	<p class="footer__navigation-mail"> in@dex.info </p>
+</nav>
+
+</section>	
+</template>
+
+
+<script>
+export default {
+	
+}
+</script>
+
+
+
+<style>
+.footer {
+	padding: 30px;
+	display: flex;
+	justify-content: space-between;
+}
+.footer__navigation {
+	margin-right: 50px;
+}
+
+.footer__navigation-link {
+	margin-bottom: 20px;
+}
+.footer__navigation-mail {
+	margin-top: 20px;
+}
+</style>
