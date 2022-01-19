1a593de4a29ad986e5d832103f9864964f54330d
soelvi <solvisimensen@live.no>
2022-01-19 09:53:40 +0100

    add About component

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+     <h1 class="about__header"> {{ Database }} </h1>
+     <div class="about__text">  </div>
+  </div>
+</template>
+ 
+<script>
+import Database from '../database.js'
+ 
+export default {
+  data() {
+     return{
+        Database
+     }
+  }
+}
+</script>
+ 
+<style>
+ 
+</style>
