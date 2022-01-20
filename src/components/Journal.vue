736f6a24cb5f993d1f5ed84c0bc8915e1ac4b429
Yngvolainen <yngve.josevold@gmail.com>
2022-01-20 12:26:27 +0100

    modified database, slideshow sorta works

--- src/components/Journal.vue
+++ src/components/Journal.vue
@@ -3,21 +3,61 @@
         <div class="journal__header">
             <h2>Journal</h2>
         </div>
-
-        <div>
-            
+        
+        <div class="journal__slideshow" @click="nextJournalIndex">
+            <img class="journal__slideshow-image" :src="journalInfo[journalIndex].image" alt="">
+            <div>
+                <p>"{{journalInfo[journalIndex].title}}"</p>
+                <p>{{journalInfo[journalIndex].location}}</p>
+                <p>{{journalInfo[journalIndex].artist}}</p>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            journalIndex: 0,
+            // hover: false
+        }
+    },
+            // https://michaelnthiessen.com/hover-in-vue/
+    computed: {
+        journalInfo() {
+            return this.$store.getters.getJournal;
+        }
+    },
+    methods: {
 
+        nextJournalIndex() {
+            if (this.journalIndex === this.journalInfo.length-1) {
+                this.journalIndex = 0
+            } else {
+                this.journalIndex++
+            }
+            // why doesn´t the code below work? 🤔
+            // this.journalIndex = (this.journalIndex === this.journalInfo.length-1) ? 0 : this.journalIndex++
+        },
+        nextImage() {
+				this.index = (this.index === this.slides.length - 1) ? 0 : this.index + 1;
+			},
+    }
 }
 </script>
 
 <style>
-    .journal__header {
+    .journal {
         text-align: center;
+    }
+
+    .journal__header {
+        /* text-align: center; */
+    }
+
+    .journal__slideshow {
+        width: 100vw;
+
     }
 </style>
--- src/database.js
+++ src/database.js
@@ -107,20 +107,20 @@ export default {
 					title: 'And My Flames Stay Till You Get Out My Way',
 					artist: 'April Bey',
 					location: 'Museum für Moderne Kunst',
-					image: 'journal_1.jpg'
+					image: '/images/journal_1.jpeg'
 				},
 				{
 					title: 'Crip Time',
 					artist: 'Kenny Fries',
 					location: 'Vienna Art Week',
-					image: 'journal_2.jpg'
+					image: '/images/journal_2.jpeg'
 				},
 				{
 					title: 'Losing Control',
 					artist: 'Alte Pinakothek',
 					location: 'Novuyo Moyo',
-					image: 'journal_3.jpg'
-				},
+					image: '/images/journal_3.jpeg'
+				}
 			],
 		
 			pages: [
@@ -138,6 +138,11 @@ export default {
 					}
 				}
 			]
+		}
+	},
+	getters: {
+		getJournal(state) {
+			return state.journal
 		}
 	}
 }
