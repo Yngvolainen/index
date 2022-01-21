fad0cf7389fa4dca3acb6f9308ec8ed7d40b0517
soelvi <solvisimensen@live.no>
2022-01-21 09:56:46 +0100

    add about component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,13 @@
 <template>
-	<div>:~)</div>
+  <div><About /></div>
 </template>
+
+<script>
+  import About from "../components/About.vue";
+ 
+  export default {
+     components: {
+        About
+     },
+  };
+</script>
