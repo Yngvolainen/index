1e05c2b7b89e73323b353ead7681c5cb7e7126a5
mimmimiro <amini.maryam@gmail.com>
2022-01-21 10:25:12 +0100

    updated

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import Home from './views/Home.vue';
-import Footer from './components/Footer.vue';
+
 
 export default [
 	{ name: 'home', path: '/', component: Home },
-	{ name: 'footer', path: '/Footer', component: Footer }
+	
 
 ];
