2a0fefe852d3fabfd7613f4d88345084e0e81ee0
Yngvolainen <yngve.josevold@gmail.com>
2022-01-24 14:44:34 +0100

    fixed the routes, again!

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import Home from './views/Home.vue';
 import FirstArticleView from './views/FirstArticleView.vue';
 import ThirdArticleView from './views/ThirdArticleView.vue';
-import About from './views/About.vue';
+import AboutView from './views/AboutView.vue';
 
 export default [
 	{ name: 'home', path: '/', component: Home },
 	{ name: 'first article', path: '/the-institute-of-the-cosmos', component: FirstArticleView },
 	{ name: 'third article', path: '/mutual-aid-social-distancing-and-dual-power-in-the-state-of-emergency', component: ThirdArticleView },	
-	{ name: 'about', 		path: '/about-us', 		component: About },
+	{ name: 'about', 		path: '/about-us', 		component: AboutView },
 ];
 
