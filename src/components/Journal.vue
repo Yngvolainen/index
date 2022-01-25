1091693ad757fba69523be81842fa74acdea1598
Yngvolainen <yngve.josevold@gmail.com>
2022-01-25 16:35:42 +0100

    visibilitychanges galore

--- src/components/Journal.vue
+++ src/components/Journal.vue
@@ -3,21 +3,27 @@
         <div class="journal__header">
             <h2>Journal</h2>
         </div>
-        <!-- mouseover hover-function not exactly...working... -->
-        <div @mouseover="hover = true" @mouseleave="hover = false" class="journal__slideshow" @click="nextJournalIndex">
-            <img class="journal__slideshow-image" :src="journalInfo[journalIndex].image" alt="artsyfartsypicture">
-            <!-- same goes for this v-if -->
-            <div v-if="hover = true" class="journal__slideshow-text">
-                <p>"{{journalInfo[journalIndex].title}}"</p>
-                <p>{{journalInfo[journalIndex].location}}</p>
-                <p>{{journalInfo[journalIndex].artist}}</p>
+
+        <div class="journal__slideshow" 
+        @click="nextJournalIndex">
+            <img class="journal__slideshow-image" :src="journalInfo.image" alt="artsyfartsypicture">
+
+            <!-- https://stackoverflow.com/questions/54936363/overlay-on-hover-in-vue-js -->
+            <div class="journal__slideshow-overlay">
+                <!-- hacke hacke -->
             </div>
+
+            <div class="journal__slideshow-text">
+                <p>"{{journalInfo.title}}"</p>
+                <p>{{journalInfo.location}}</p>
+                <p>{{journalInfo.artist}}</p>
+            </div>
+
             <div class="journal__slideshow-indicators">
-                <span 
-                    v-for="(item, index) in journalInfo.length" :key="item.index">
+                <span v-for="(item, index) in fromJournalData.length" :key="item.id">
                     <button class="journal__slideshow-indicators--inactive"
-                        :class=" index === journalIndex ? 'journal__slideshow-indicators--active' : ''"
-                        @click="journalIndex = index-1">
+                        :class="index === journalIndex ? 'journal__slideshow-indicators--active' : ''"
+                        @click="shiftJournalIndex(index)">
                         <!-- above @click gives TypeError on first button 🤔 ,
                         but does not break page.
                         It´s probably beacause -1 is not valid at first index. 
@@ -40,27 +46,30 @@ export default {
         }
     },
     computed: {
-        journalInfo() {
+        fromJournalData() {
             return this.$store.getters.getJournal;
+        },
+        journalInfo() {
+            return this.fromJournalData[this.journalIndex]
         }
+       
     },
     methods: {
         nextJournalIndex() {
-            if (this.journalIndex === this.journalInfo.length-1) {
-                this.journalIndex = 0
-            } else {
-                this.journalIndex++
-            }
-            // why doesn´t the code below work? 🤔
-            // this.journalIndex = (this.journalIndex === this.journalInfo.length-1) ? 0 : this.journalIndex++
+           this.journalIndex === this.fromJournalData.length-1 ? this.journalIndex = 0 : this.journalIndex++
         },
-        setItemRef() {
-            return this.journalIndex
+
+        shiftJournalIndex(index) {
+            this.journalIndex = index - 1;
+        },
+
+        mouseOver() {
+            this.hover = !this.hover
         }
     },
-    updated() {
-        console.log(this.journalIndex)
-    }
+    // updated() {
+    //     console.log(this.journalIndex)
+    // }
 }
 </script>
 
@@ -70,43 +79,57 @@ export default {
     }
 
     .journal__header {
-        margin-bottom: 40px;
+        margin-bottom: var(--lineheight-medium);
     }
 
     .journal__slideshow {
         width: 100vw;
         position: relative;
     }
-/* 
-    .journal__slideshow-image {
-        position: relative;
-    } */
+
+    .journal__slideshow-overlay {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        background: black;
+        opacity: 0.2;
+        transition: .5s ease;
+        /* z-index: 999; */
+    }
+
+ 
+
+    .journal__slideshow:hover div {
+        visibility: visible;
+    }
 
     .journal__slideshow-text {
+        visibility: visible;
         color: #FFFFFF;
         position: absolute;
+        /* https://stackoverflow.com/questions/37721175/css-how-to-center-text-vertically-and-horizontally-over-an-image/37721341 */
         top: 50%;
         left: 50%;
-        /* https://stackoverflow.com/questions/37721175/css-how-to-center-text-vertically-and-horizontally-over-an-image/37721341 */
         transform: translate(-50%, -50%);
     }
 
     .journal__slideshow-indicators {
         color: white;
         position: absolute;
-        bottom: 1.5rem;
+        bottom: 2rem;
         left: 50%;
         transform: translate(-50%);
     }
 
     .journal__slideshow-indicators button{
+        visibility: visible;
         cursor: default;
         margin-right: 1rem;
     }
-
-    /* .journal__slideshow-indicators button:last-child{
-        margin-right: 0;
-    } */
 
     .journal__slideshow-indicators--inactive {
         width: 1rem;
@@ -114,10 +137,33 @@ export default {
         border: 1px solid white;
         border-radius: 50%;
         background: transparent;
-
     }
 
     .journal__slideshow-indicators--active {
         background: white;
     }
+
+    @media screen and (min-width: 769px) {
+    .journal__slideshow div {
+        visibility: hidden;
+    }
+
+    .journal__slideshow-overlay {
+        opacity: 0;
+    }
+
+    .journal__slideshow-overlay:hover {
+        opacity: 0.2;
+    }
+
+    .journal__slideshow-text {
+        visibility: hidden;
+        transition: .5s ease;
+    }
+
+    .journal__slideshow-text:hover {
+        background: rgba(0, 0, 0, 0.2);
+        transition: .5s ease;
+    }
+}
 </style>
