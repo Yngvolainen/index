9f19dc879485f9cebb3d7fd5a1afb994527cf21f
lstubdal <lotte.stubdal@gmail.com>
2022-01-26 10:07:57 +0100

    fix header and leads

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -39,59 +39,58 @@
 
 <style>
    .article {
-      
-      display: grid;
-      grid-template-columns: repeat(12, 1fr);
+     /*  display: grid;
+      grid-template-columns: repeat(12, 1fr); */
       text-align: center;
       gap: var(--column-gap);
-      margin-bottom: var(--lineheight-big);
-      grid-column: span 13;   /* for grid in ArticlesHomepage.vue */
+      margin-bottom: var(--lineheight-big);  
+      grid-column: span 12;
    } 
 
    .article__title {
-      grid-column: span 13;
+      grid-column: span 12;
       font-size: var(--heading);
-      padding-bottom: var(--lineheight-medium);
+      padding-bottom: var(--lineheight-small);
    }
 
    .article__lead {
-      grid-column: span 13;   
+      grid-column: span 12;   
       font-size: var(--caption);
       padding-bottom: var(--lineheight-small); 
    } 
 
    .article__image {
-      grid-column: span 13;
+      grid-column: span 12;
    }
 
-   .article__layout--first {
+   .article__layout--first,
+   .article__layout--third {
       cursor: pointer;
    }
 
    @media screen and (min-width: 968px) {    /* desktop */
-     .article__layout--second {
+      .article__layout--second {
          grid-column: 1 / span 6;
       }
       .article__layout--third {
-         cursor: pointer;
-         grid-column: 7/ span 6;
+         grid-column: 7 / span 6;
       }
+      .article__title--first {
+        grid-column: 4 / span 6;
+      } 
 
-      .article__title--second {
-        grid-column: 2 / span 8;
+      .article__title--second, 
+      .article__lead--second {
+        grid-column: 2 / span 4;
       }
 
       .article__title--third, 
       .article__lead--third {
-        grid-column: 4 / span 8;
+        grid-column: 8 / span 4;
       }
 
       .article__lead--first {
         grid-column: 4 / span 6;
-      }
-
-      .article__lead--second {
-        grid-column: 2 / span 8;
       }
 
      .article__image--first {
@@ -99,11 +98,11 @@
      }
 
       .article__image--second {
-        grid-column: 1 / span 12;
+        grid-column: 1 / span 6;
       }
 
       .article__image--third {
-        grid-column: 1 / span 12;
+        grid-column: 7 / span 6;
       }
    }
 </style>
