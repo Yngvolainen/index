06daaff5e179da55c3530bdea3bc2a1b717b39d4
lstubdal <lotte.stubdal@gmail.com>
2022-01-26 17:02:16 +0100

    add announcement to articles

--- src/components/ArticlesHomepage.vue
+++ src/components/ArticlesHomepage.vue
@@ -2,55 +2,30 @@
    <div class="articlesHomepage">
       <h2 class="articlesHomepage__header"> {{ header }}</h2>
 
-      <RouterLink  class="article" :class="'article__' + index" v-for="(article, index) in articles" :to="{ name: 'article', params: {article_slug: article.slug }}">
-         <h3 class="article__title" :class="'article__title-' + index"> {{ article.title }} </h3>
-         <p class="article__lead" :class="'article__lead-' + index"> {{ article.lead }} </p>
-         <img class="article__image" :src="article.preview.image"/>
-      </RouterLink>
-   
-      
-
-<!--       <div v-for="(article, index) in articles" class="article" :class="'article__' + index">
+      <div v-for="(article, index) in articles" class="article" :class="'article__' + index">
          <router-link :to="{ name: 'article', params: {article_slug: article.slug }}" class="article__link">
             <h3 class="article__title" :class="'article__title-' + index"> {{ article.title }}</h3>
             <p class="article__lead" :class="'article__lead-' + index"> {{ article.lead }}</p>
          </router-link>
 
          <img class="article__image" :src="article.preview.image"/>
-   </div> --> 
+      </div>  
    </div>
 </template>
 
 <script>
-/*    import Article from './Article.vue'; */
-
    export default {
       data() {
          return {
             header: 'Latest'
          }
       }, 
-/* 
-      components: {
-         Article
-      }, */
 
       computed: {
          articles() {
             return this.$store.getters.getArticles
          },
       }, 
-
-/*       methods: {
-         seeFullArticle(article) {
-            if (article.title === this.articles[0].title) {
-               this.$router.push('/' + this.articles[0].slug) /* pushes new entry into history stack 
-
-            } else if (article.title === this.articles[2].title) {
-               this.$router.push('/' + this.articles[2].slug);
-            }
-         }, 
-      }*/
    }
 
 </script>
@@ -67,17 +42,18 @@
       grid-column: 6 / span 2;
       text-align: center;
       font-size: var(--body);
+      margin-bottom: var(--bottom-medium);
    }
 
    .article {
- /*      grid-column: span 12;
-      display: grid;
-      grid-template-columns: var(--grid-column-12); */
-
       text-align: center;
       margin-bottom: var(--bottom-big);
       text-decoration: none;
    } 
+
+   .article__link {
+      text-decoration: none;
+   }
 
    .article__title {
       color: var(--primary);
@@ -91,7 +67,7 @@
       color: var(--primary);
       grid-column: span 12;    
       font-size: var(--caption);
-      padding-bottom: var(--bottom-small); 
+      padding-bottom: var(--bottom-medium); 
    }
 
    .article__image {
@@ -99,15 +75,14 @@
    }
 
    @media screen and (min-width: 968px) {    /* desktop */
-      .article__0 {
+       .article__0 {
          grid-column: 3 / span 8;
       }
+       
 
       .article__lead-0 {
-         color: var(--primary);
          grid-column: 4 / span 6;   
-         font-size: var(--caption);
-         padding-bottom: var(--bottom-small); 
+
       }
 
       .article__1 {
--- src/views/FullArticleView.vue
+++ src/views/FullArticleView.vue
@@ -1,19 +1,23 @@
 <template>
     <Header />
+    <Announcement />
     <FullArticle />
     <Footer />
 </template>
 
 <script>
-    import FullArticle from '../components/FullArticle.vue';
     import Header from '../components/Header.vue';
+    import Announcement from '../components/Announcement.vue';
+    import FullArticle from '../components/FullArticle.vue';
     import Footer from '../components/Footer.vue';
-
+    
     export default {
         components: {
-            FullArticle,
             Header,
-            Footer
+            Announcement,
+            FullArticle,
+            Footer,
+            
         }
     }
 </script>
